cbdf8d7d2545e7d1a589fc039358749267dd51d6
pooyan <thepooyan@gmail.com>
2025-01-08 15:03:59 +0330

    nothing!

--- vite-project/src/LiteValidate.ts
+++ vite-project/src/LiteValidate.ts
@@ -1,48 +1,66 @@
 import messages from "./default-messages.json";
 import rules from "./default-rules.ts";
-const TRIGGER_KEYWORD = "validate";
-const ERROR_CLASSNAME = "validation-error";
+const config = {
+  TRIGGER_KEYWORD: "validate",
+  ERROR_CLASSNAME: "validation-error",
+  SUPPORTED_ELEMENTS: ["input", "textarea", "select"],
+};
+
+function selectTargetElements() {
+  let selection = ``;
+  for (let s of config.SUPPORTED_ELEMENTS) {
+    selection += `${s}[data-${config.TRIGGER_KEYWORD}],`;
+  }
+  return selection.substring(0, selection.length - 1);
+}
 
 export default function init() {
-  let items = document.querySelectorAll(`[data-${TRIGGER_KEYWORD}]`) as NodeListOf<HTMLElement>;
+  let items = document.querySelectorAll(
+    selectTargetElements(),
+  ) as NodeListOf<HTMLElement>;
   SetValidationEvents(items);
 }
 
 export function SetValidationEvents(items: NodeListOf<HTMLElement>) {
-  items.forEach(i => {
+  items.forEach((i) => {
     setValidationEvent(i);
-  })
+  });
 }
 
 function setValidationEvent(item: HTMLElement) {
   item.addEventListener("blur", () => {
     validateItem(item);
-  })
+  });
 }
 
 export function validateSection(section: HTMLElement) {
-  (section.querySelectorAll(`[data-${TRIGGER_KEYWORD}]`) as NodeListOf<HTMLElement>).forEach(i => validateItem(i));
+  (
+    section.querySelectorAll(selectTargetElements()) as NodeListOf<HTMLElement>
+  ).forEach((i) => validateItem(i));
 }
 
 function validateItem(item: HTMLElement) {
   let success = true;
   function checkErrorElement() {
-    if (item.nextElementSibling?.classList.contains(ERROR_CLASSNAME)) return
+    if (item.nextElementSibling?.classList.contains(config.ERROR_CLASSNAME))
+      return;
     let el = document.createElement("div");
-    el.classList.add(ERROR_CLASSNAME);
+    el.classList.add(config.ERROR_CLASSNAME);
     item.insertAdjacentElement("afterend", el);
   }
   function findValidations() {
-    return (item.dataset[TRIGGER_KEYWORD] || "").split(" ")
+    return (item.dataset[config.TRIGGER_KEYWORD] || "").split(" ");
   }
   checkErrorElement();
   let validations = findValidations();
-  if (validations.length == 0) return
-  validations.forEach(v => {
+  if (validations.length == 0) return;
+  validations.forEach((v) => {
     let read = rules[v];
-    if (!read) throw new Error(`No rule defined for "${v}".`)
-    let res = read(item.value)
+    if (!read) throw new Error(`No rule defined for "${v}".`);
+    let res = read(item.value);
     if (res === false) success = false;
-  })
-  return success
+  });
+  return success;
 }
+
+function validationFailed() { }
